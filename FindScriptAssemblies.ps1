ba87dac98af821c37390319d4e67dceb91d15feb
R00tDroid <info@rootdroid.nl>
2021-12-29 22:12:10 +0100

    Remove unused scripts

--- FindScriptAssemblies.ps1
+++
@@ -1,5 +0,0 @@
-$EngineLocation = $args[0]
-$LibCache = Get-ChildItem "$EngineLocation\\Editor\\Data\Resources\\PackageManager\\ProjectTemplates\\libcache" -filter "*.3d-*" -Directory
-$LibCachePath = $LibCache.FullName
-
-Write-Host "$LibCachePath\ScriptAssemblies"
