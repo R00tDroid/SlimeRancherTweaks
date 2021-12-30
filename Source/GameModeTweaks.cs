abe30219461d82f4f69c403e1df807d4c80f4d85
R00tDroid <info@rootdroid.nl>
2021-12-30 19:53:43 +0100

    Implement InstantUpgrades and ReceiveMails

--- Source/GameModeTweaks.cs
+++ Source/GameModeTweaks.cs
@@ -8,11 +8,15 @@ namespace SRTweaks
     {
         public static bool AllowTarrSpawns = true; // Default true;
         public static bool SuppressTutorials = false; // Default false;
+        public static bool InstantUpgrades = false; // Default false;
+        public static bool ReceiveMails = true; // Default true;
 
         public override void PreLoad()
         {
             SRML.Console.Console.RegisterCommand(new SetTarrSpawnCommand());
             SRML.Console.Console.RegisterCommand(new SetSuppressTutorialsCommand());
+            SRML.Console.Console.RegisterCommand(new SetInstantUpgradesCommand());
+            SRML.Console.Console.RegisterCommand(new SetReceiveMailsCommand());
         }
 
         public override void GameLoaded()
@@ -24,18 +28,24 @@ namespace SRTweaks
             // Set Gamemode spawning
             SRSingleton<SceneContext>.Instance.GameModeConfig.GetModeSettings().preventHostiles = !AllowTarrSpawns;
             SRSingleton<SceneContext>.Instance.GameModeConfig.GetModeSettings().assumeExperiencedUser = SuppressTutorials;
+            SRSingleton<SceneContext>.Instance.GameModeConfig.GetModeSettings().immediateUpgrades = InstantUpgrades;
+            SRSingleton<SceneContext>.Instance.GameModeConfig.GetModeSettings().suppressStory = !ReceiveMails;
         }
 
         public override void SaveSettings(CompoundDataPiece data)
         {
             data.SetValue("AllowTarrSpawns", AllowTarrSpawns);
             data.SetValue("SuppressTutorials", SuppressTutorials);
+            data.SetValue("InstantUpgrades", InstantUpgrades);
+            data.SetValue("ReceiveMails", ReceiveMails);
         }
 
         public override void LoadSettings(CompoundDataPiece data)
         {
             AllowTarrSpawns = Main.GetSaveValue<bool>(data, "AllowTarrSpawns", true);
             SuppressTutorials = Main.GetSaveValue<bool>(data, "SuppressTutorials", false);
+            InstantUpgrades = Main.GetSaveValue<bool>(data, "InstantUpgrades", false);
+            ReceiveMails = Main.GetSaveValue<bool>(data, "ReceiveMails", true);
         }
 
         private ITweakSettingsUI SettingsUI = new GameModeTweaksSettingsUI();
@@ -49,6 +59,8 @@ namespace SRTweaks
     {
         private bool allowTarrSpawns;
         private bool suppressTutorials;
+        private bool instantUpgrades;
+        private bool receiveMails;
 
         public override void OnGUI()
         {
@@ -57,18 +69,24 @@ namespace SRTweaks
 
             allowTarrSpawns = GUILayout.Toggle(allowTarrSpawns, "Allow Tarr to spawn (default: true)");
             suppressTutorials = GUILayout.Toggle(suppressTutorials, "Suppress tutorials (default: false)");
+            instantUpgrades = GUILayout.Toggle(instantUpgrades, "Upgrades instantly available (default: false)");
+            receiveMails = GUILayout.Toggle(receiveMails, "Receive mails (default: true)");
         }
 
         public override void Load()
         {
             allowTarrSpawns = GameModeTweaks.AllowTarrSpawns;
             suppressTutorials = GameModeTweaks.SuppressTutorials;
+            instantUpgrades = GameModeTweaks.InstantUpgrades;
+            receiveMails = GameModeTweaks.ReceiveMails;
         }
 
         public override void Save()
         {
             GameModeTweaks.AllowTarrSpawns = allowTarrSpawns;
             GameModeTweaks.SuppressTutorials = suppressTutorials;
+            GameModeTweaks.InstantUpgrades = instantUpgrades;
+            GameModeTweaks.ReceiveMails = receiveMails;
         }
     }
 
@@ -117,6 +135,56 @@ namespace SRTweaks
             }
 
             GameModeTweaks.SuppressTutorials = newValue;
+            Main.ApplySettings();
+            return true;
+        }
+    }
+
+    public class SetInstantUpgradesCommand : ConsoleCommand
+    {
+        public override string Usage => "instantupgrades [True/False]";
+        public override string ID => "instantupgrades";
+        public override string Description => "gets or sets whether upgrades will be available instantly";
+
+        public override bool Execute(string[] args)
+        {
+            if (args == null || args.Length < 1)
+            {
+                Main.Log("Are upgrades instantly available: " + GameModeTweaks.InstantUpgrades + " (default: False)");
+                return true;
+            }
+
+            if (!bool.TryParse(args[0], out bool newValue))
+            {
+                return false;
+            }
+
+            GameModeTweaks.InstantUpgrades = newValue;
+            Main.ApplySettings();
+            return true;
+        }
+    }
+
+    public class SetReceiveMailsCommand : ConsoleCommand
+    {
+        public override string Usage => "receivemails [True/False]";
+        public override string ID => "receivemails";
+        public override string Description => "gets or sets whether mails will be received";
+
+        public override bool Execute(string[] args)
+        {
+            if (args == null || args.Length < 1)
+            {
+                Main.Log("Will receive mails: " + GameModeTweaks.ReceiveMails + " (default: True)");
+                return true;
+            }
+
+            if (!bool.TryParse(args[0], out bool newValue))
+            {
+                return false;
+            }
+
+            GameModeTweaks.ReceiveMails = newValue;
             Main.ApplySettings();
             return true;
         }
